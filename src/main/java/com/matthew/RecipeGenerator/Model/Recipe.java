a2ea9e7866e267e41d9b89d165433bb718fa7dc5
Matthew <matthew.darrow3@gmail.com>
2025-02-07 17:01:23 -0500

    finished models

--- src/main/java/com/matthew/RecipeGenerator/Model/Recipe.java
+++ src/main/java/com/matthew/RecipeGenerator/Model/Recipe.java
@@ -1,4 +1,37 @@
 package com.matthew.RecipeGenerator.Model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "recipes")
 public class Recipe {
+
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Getter
+    @Setter
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Getter
+    @Setter
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Getter
+    @Setter
+    @Column(name = "instructions", nullable = false)
+    private String instructions;
 }
--- src/main/java/com/matthew/RecipeGenerator/Model/RecipeIngredient.java
+++ src/main/java/com/matthew/RecipeGenerator/Model/RecipeIngredient.java
@@ -1,19 +1,44 @@
 package com.matthew.RecipeGenerator.Model;
 
-import java.sql.Timestamp;
-
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "recipe_ingredients")
 public class RecipeIngredient {
 
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "recipe_id", nullable = false)
+    private Recipe recipe;
+
+    @Getter
+    @Setter
+    @ManyToOne
+    @JoinColumn(name = "ingredient_id", nullable = false)
+    private Ingredient ingredient;
+
+    @Getter
+    @Setter
+    @Column(name = "quantity", nullable = false)
+    private String quantity; // Stores the amount (e.g., "200", "2", "1/2").
+
+    @Getter
+    @Setter
+    @Column(name = "unit", nullable = false)
+    private String unit; // Stores the measurement unit (e.g., "grams", "cups", "tablespoons").
 }
