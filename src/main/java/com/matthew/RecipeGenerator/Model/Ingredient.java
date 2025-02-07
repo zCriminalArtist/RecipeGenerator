c0aa51ed0a859e2a78f3b21395a8b4748572358d
Matthew <matthew.darrow3@gmail.com>
2025-02-07 16:49:40 -0500

    finished models

--- src/main/java/com/matthew/RecipeGenerator/Model/Ingredient.java
+++ src/main/java/com/matthew/RecipeGenerator/Model/Ingredient.java
@@ -1,5 +1,32 @@
 package com.matthew.RecipeGenerator.Model;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ingredients")
 public class Ingredient {
+
+    @Getter
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Getter
+    @Setter
+    @Column(name = "name", nullable = false, unique = true)
+    private String name;
+
+    @Getter
+    @Setter
+    @Column(name = "category", nullable = false)
+    private String category;
 
 }
